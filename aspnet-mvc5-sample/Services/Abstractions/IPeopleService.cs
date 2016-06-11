ac8386bc3c8e82ab21ba0909b213588767052053
121jigowatts <sh.yoshida@live.jp>
2016-06-12 02:17:07 +0900

    Web APIによる実装に変更

---
+++ aspnet-mvc5-sample/Services/Abstractions/IPeopleService.cs
@@ -0,0 +1,15 @@
+﻿using aspnet_mvc5_sample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aspnet_mvc5_sample.Services.Abstractions
+{
+    public interface IPeopleService
+    {
+        IEnumerable<Person> GetPeopleByName(String name);
+
+    }
+}
