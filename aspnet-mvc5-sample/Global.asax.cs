a8ba26f5b90968d991268c8ee10bc1d6970fa7c5
121jigowatts <sh.yoshida@live.jp>
2017-02-10 05:29:47 +0000

    グローバルキャッシュサービスの注入

--- aspnet-mvc5-sample/Global.asax.cs
+++ aspnet-mvc5-sample/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using aspnet_mvc5_sample.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,8 +12,29 @@ namespace aspnet_mvc5_sample
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        // キャッシュラッパーオブジェクトへの内部参照
+        private static ICacheService _internalCacheObject;
+
+        public  static void RegisterCacheService(ICacheService cacheService)
+        {
+            _internalCacheObject = cacheService;
+        }
+
+        // コントローラーメソッドからキャッシュオブジェクトにアクセスするためのプロパティ
+        // Cacheオブジェクトの代わり
+        public static ICacheService CacheService
+        {
+            get { return _internalCacheObject; }
+        }
+
         protected void Application_Start()
         {
+            // グローバルキャッシュサービスの注入
+            RegisterCacheService(new AspNetCacheService());
+
+            // アプリケーションスコープのデータをキャッシュ
+            CacheService["StartTime"] = DateTime.Now;
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
